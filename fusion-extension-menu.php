18f0e6771eb0fdf7004af439e35ec4a71e27f275
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 14:14:56 -0300

    Fix stylesheet enqueuing bug

--- fusion-extension-menu.php
+++ fusion-extension-menu.php
@@ -66,14 +66,11 @@ class FusionExtensionMenu	{
 	 *
 	 */
 	 
-	 public function front_enqueue_scripts_styles() {
+	public function front_enqueue_scripts_styles() {
 		wp_register_script('bootstrap_hover_dropdown', plugin_dir_url( __FILE__ ) . 'includes/js/bootstrap-hover-dropdown.min.js', array('jquery'), '2.1.3', true);
 		//plugin
 		wp_register_script( 'fsn_menu', plugin_dir_url( __FILE__ ) . 'includes/js/fusion-extension-menu.js', array('jquery'), '1.0.0', true );
-		global $post;
-		if (has_shortcode($post->post_content, 'fsn_menu')) {
-			wp_enqueue_style( 'fsn_menu', plugin_dir_url( __FILE__ ) . 'includes/css/fusion-extension-menu.css', false, '1.0.0' );
-		}
+		wp_enqueue_style( 'fsn_menu', plugin_dir_url( __FILE__ ) . 'includes/css/fusion-extension-menu.css', false, '1.0.0' );
 	}
 	
 }
