7dc7dfe017ce346c89241afe848d106c0f3c5bde
Jeff Chew <jeff.chew@agencydominion.com>
2017-08-21 17:58:41 -0300

    Update version number

--- fusion-extension-menu.php
+++ fusion-extension-menu.php
@@ -6,7 +6,7 @@
  * Plugin Name: Fusion : Extension - Menu
  * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Menu Extension Package for Fusion.
- * Version: 1.1.10
+ * Version: 1.1.11
  * Author: Agency Dominion
  * Author URI: http://agencydominion.com
  * Text Domain: fusion-extension-menu
